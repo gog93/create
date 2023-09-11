f0f7b043d77a3e220eb423f0d6f9d4279ff968fe
gohar93 <zakaryangohar49@gmail.com>
2023-09-11 06:13:19 -0700

    add acupuncture

--- app/src/main/java/com/example/create/AcupunctureActivity.java
+++ app/src/main/java/com/example/create/AcupunctureActivity.java
@@ -13,7 +13,7 @@ public class AcupunctureActivity  extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.experiment_page_activity);
+        setContentView(R.layout.acupuncture_activity);
         dbHelper = new DbHelper(this);
 
         TextView acupunctureTextView = findViewById(R.id.acupuncture);
